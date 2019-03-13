93b40030cc754cf337ab8efbe0388733c574d6ed
Samuel <samuel.courthial.18@gmail.com>
2019-03-13 15:58:26 +0100

    End of tp1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+interface Movie {
+  title: string ;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  movies: Movie[] = [] ;
 
+  getMovies(input: string): void {
+    if (input.length >= 3) {
+      this.movies.push({title: input}) ;
+    } else {
+      this.movies = [] ;
+    }
+  }
 }
